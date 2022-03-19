14da7352ffe82a32391158e05c206424bb9f96df
larissa moura basso <larissa.mbasso@outlook.com>
2022-03-19 15:53:26 -0300

    arrumar footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="footer">
-    <v-footer padless>
-      <v-col
-        class="text-center"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-col>
-    </v-footer>
-  </div>
+  <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </v-col>
+  </v-footer>
 </template>
 
 <script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,7 +26,7 @@
               <v-list-item-icon>
                 <v-icon>mdi-home</v-icon>
               </v-list-item-icon>
-              <v-list-item-title>Sobre o APP</v-list-item-title>
+              <v-list-item-title>Inicio</v-list-item-title>
             </v-list-item>
 
             <v-list-item>
@@ -34,6 +34,18 @@
                 <v-icon>mdi-account</v-icon>
               </v-list-item-icon>
               <v-list-item-title>Logar</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Produtos</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Quem somos</v-list-item-title>
             </v-list-item>
           </v-list-item-group>
         </v-list>
