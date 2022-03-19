d4038f8dd96cc8bac361a51bec288ae5b710e3d2
Mariana Thomaz <thomaz.mariana@gmail.com>
2022-03-19 12:54:36 -0300

    QuemSomos

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TelaLogin from '../views/TelaLogin.vue'
+import QuemSomos from '../views/QuemSomos.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/TelaLogin',
+    name: 'TelaLogin',
+    component: TelaLogin
+  },
+  {
+    path: '/QuemSomos',
+    name: 'QuemSomos',
+    component: QuemSomos
   }
 ]
 
