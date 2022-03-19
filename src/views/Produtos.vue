2b8c1a05371a257938b7b516025461f58ff9db00
Gustavo Urzua <gustavo.urzua2001@gmail.com>
2022-03-19 13:43:58 -0300

    Atualização dos cards da Tela de Produtos

--- src/views/Produtos.vue
+++ src/views/Produtos.vue
@@ -10,24 +10,40 @@
                     gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
                     height="200px"
                     >
-                    <v-card-title v-text="produto.nome"></v-card-title>
                     </v-img>
 
+                    <v-card-title v-text="produto.nome"></v-card-title>
+
                     <v-card-actions>
-                    <v-spacer></v-spacer>
-
                     <v-btn icon>
-                        <v-icon>mdi-heart</v-icon>
+                        <v-icon>mdi-cart</v-icon>
                     </v-btn>
+                    <v-card-subtitle>
+                        R$: {{produto.preco}}
+                    </v-card-subtitle>
+                        <v-btn
+                            color="orange lighten-2"
+                            text>
+                            Detalhes
+                        </v-btn>
 
-                    <v-btn icon>
-                        <v-icon>mdi-bookmark</v-icon>
-                    </v-btn>
-
-                    <v-btn icon>
-                        <v-icon>mdi-share-variant</v-icon>
-                    </v-btn>
+                        <v-btn
+                            icon
+                            @click="show = !show"
+                        >
+                            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
                     </v-card-actions>
+                    <v-expand-transition>
+                        <div v-show="show">
+                            <v-divider></v-divider>
+
+                            <v-card-text>
+                                <p>Vendedor: {{produto.local.nome}}</p>
+                                <p>Endereço: {{produto.local.endereco}}</p>
+                            </v-card-text>
+                        </div>
+                        </v-expand-transition>
                 </v-card>
                 </v-col>
                 </v-row>
@@ -42,7 +58,7 @@
         return {
         listaProdutos: [],
         url: "https://it3-hbn-default-rtdb.firebaseio.com/ovosPascoa.json",
-        show: false,
+        show: false
         };
     },
     created() {
@@ -56,7 +72,4 @@
     </script>
 
     <style scoped>
-        .mx-auto{
-            
-        }
     </style>
